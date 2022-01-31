2be315153a0d68e08e22af84ed7b7f80f5795099
Kingston <kingston@vanlaak.cc>
2022-01-31 10:59:48 +0100

    order history

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -1,43 +1,30 @@
 <?php
-
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Cart;
-use App\Product;
+use App\Products;
 use App\Order;
-use App\User;
 use Auth;
 
 class CartController extends Controller
 {
+
+
+
     /**
      * addToCart function send request and productId
      */
-    public function addToCart(Request $request, $productId)
-    {
-        // Trying to get product from the database with the eloquent method 'find'
-        $product = Product::find($productId);
-        /* Check if cart already exists.
-        If the cart does exist it wil retrieve it.
-        If the cart does not exist, it will be null */
-        $cart = new Cart($request);
+    public function addToCart(Request $request, $productId){
+        $product = Products::find($productId);
+        $cart =  new Cart($request);
         $cart->addToCart($product, $product->id);
-
-        $request->session()->put('cart.index', $cart);
-        return view('cart.index');
-    }
-
-    public function getModelProd($productIdArr)
-    {
-        $products = Product::whereIn('id', $productIdArr)->get();
-        return $products;
+        return redirect()->to('cart');
     }
 
     /**
      * getCart function get the products en return them with view to the cart
      */
-    public function getCart(Request $request)
-    {
+    public function getCart(Request $request) {
         if (!$request->session()->has('cart')) {
             return view('cart.index', ['products' => null]);
         }
@@ -49,20 +36,18 @@ class CartController extends Controller
     /**
      * Updatecart function uppdates the qty of a product
      */
-    public function updateCart(Request $request)
-    {
+    public function updateCart(Request $request){
         $qty = $request->input('qty');
         $productId = $request->input('productId');
         $cart = new Cart($request);
-        $cart->updateCart($productId, $qty);
-        return redirect()->to('cart.index');
+        $cart->updateCart($productId,$qty);
+        return redirect()->to('cart');
     }
 
     /**
      * removeCart function removes a specifically product trough the product id
      */
-    public function removeCart(Request $request, $productId)
-    {
+    public function removeCart(Request $request, $productId){
         $cart = new Cart($request);
         $cart->removeCart($productId);
         return redirect()->to('cart');
@@ -71,8 +56,7 @@ class CartController extends Controller
     /**
      * checkout function go to checkout page
      */
-    public function checkout(Request $request)
-    {
+    public function checkout(Request $request){
         if (!$request->session()->has('cart')) {
             return view('checkout.index', ['products' => null]);
         }
@@ -85,16 +69,12 @@ class CartController extends Controller
     /**
      * order submit function stores the order in the DB and redirect to orderPlaced page
      */
-    public function orderSubmit(Request $request)
-    {
+    public function orderSubmit(Request $request){
         $cart = new Cart($request);
         $order = new Order();
         $order->cart = serialize($cart);
-//        dd(json_decode($order->cart));
         $order->address = $request->input('address');
         $order->name = $request->input('name');
-        $idCust = $request->session()->all()['_token'];
-        $order->user_id = $idCust;
         $order->save();
 
         $request->session()->forget('cart');
@@ -102,14 +82,4 @@ class CartController extends Controller
         return redirect()->route('home');
     }
 
-    public function viewOrder(Request $request)
-    {
-        $orders = Auth::user()->orders;
-        //  dd(Auth::user());
-        $orders->transform(function ($order, $key) {
-            $order->cart = unserialize($order->cart);
-            return $order;
-        });
-        return view('order-view.index', ['orders' => $orders]);
-    }
 }
